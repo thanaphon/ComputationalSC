077916d6310e052646f229ee8ff36121bce35db0
thanaphon <thanaphon.tang@gmail.com>
2019-01-24 09:28:07 +0700

    Revert "'HW-folder'"
    This reverts commit bd61504cd83731d05166fed88b25779bdd73a5d9.
    
    Backtrack

--- Ohio/code/myexpmatrix.m
+++
@@ -1,9 +0,0 @@
-function A = myexpmatrix(m,r)
-    % produces the matrix for the explicit method for a parabolic equation
-    % Inputs: m -- the size of the matrix
-    %         r -- the main parameter, ck/h^2
-    % Output: A -- an m by m matrix
-    u = (1-2*r)*ones(m,1);  % make a vector for the main diagonal
-    v = r*ones(m-1,1);    % make a vector for the upper and lower diagonals
-    A = diag(u) + diag(v,1) + diag(v,-1);  % assemble
-end
